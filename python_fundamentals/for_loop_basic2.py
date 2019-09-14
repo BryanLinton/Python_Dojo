7930580ddc7f6a9439c44b7e075b507562035002
BryanLinton <53503899+BryanLinton@users.noreply.github.com>
2019-09-14 14:19:30 -0700

    for loop basic update

--- python_fundamentals/for_loop_basic2.py
+++ python_fundamentals/for_loop_basic2.py
@@ -1,76 +1,104 @@
-# def biggie_size(l):
-#     for i in range(0, len(l), 1):
-#         if l[i] > 0:
-#             l[i] = "big"
-#     return l        
-# print(biggie_size([-1, 3, 5, -5]))
+def biggie_size(l):
+    for i in range(0, len(l), 1):
+        if l[i] > 0:
+            l[i] = "big"
+    return l        
+print(biggie_size([-1, 3, 5, -5]))
 
-# def count_positives(l):
-#     val = 0
-#     for i in range (0, len(l), 1):
-#         if l[i] > 0:
-#             val = val + l[i]
-#     l[len(l) - 1] = val
-#     return l    
-# print(count_positives([-1, 1, 1, 1]))   
+def count_positives(l):
+    val = 0
+    for i in range (0, len(l), 1):
+        if l[i] > 0:
+            val = val + l[i]
+    l[len(l) - 1] = val
+    return l    
+print(count_positives([-1, 1, 1, 1]))
+print(count_positives([1, 6, -4, -2, -7, -2]))   
 
-# def sum_total(l):
-#     sum = 0
-#     for i in range (0, len(l), 1):
-#         sum = sum + l[i]
-#     return sum
-# print(sum_total([1,2,3,4])) 
+def sum_total(l):
+    sum = 0
+    for i in range (0, len(l), 1):
+        sum = sum + l[i]
+    return sum
+print(sum_total([1,2,3,4]))
+print(sum_total([6, 3, -2])) 
 
-# def average(l):
-#     sum = 0
-#     for i in range(0, len(l), 1):
-#         sum = sum + l[i]
-#     avg = sum / len(l)    
-#     return avg 
-# print(average([1, 2, 3, 4])) 
+def average(l):
+    total = 0
+    for i in range(0, len(l), 1):
+        total = total + l[i]
+    float(total) / float(len(l))    
+    return 
+print(average([1, 2, 3, 4])) 
  
-# def length(l):
-#     long = 0
-#     for i in range (1, len(l) + 1 , 1):
-#         long = long + 1
-#     return long    
-# print(length([1,2,3,4]))
+def length(l):
+    long = 0
+    for i in range (1, len(l) + 1 , 1):
+        long = long + 1
+    return long    
+print(length([1,2,3,4]))
+print(length([]))
  
-# def minimum(l):
-#     small = l[0]
-#     for i in range (0, len(l), 1):
-#         if l[i] < small:
-#             small = l[i]
-#     return small
-# print(minimum([1,-3,3,-1]))  
+def minimum(l):
+    if len(l) == 0:
+        return False
 
-# def maximum(l):
-#     large = l[0]
-#     for i in range (0, len(l), 1):
-#         if l[i] > large:
-#             large = l[i]
-#     return large
-# print(maximum([1,-3,3,-1]))
+    small = l[0]
+    for i in range (0, len(l), 1):
+        if l[i] < small:
+            small = l[i]
+    return small
+print(minimum([1,-3,3,-1]))
+print(minimum([]))
 
-# def ultimate_anal(l):
-#     maximum = l[0]
-#     minimum = l[0]
-#     total = 0
-#     length = len(l)
-#     for i in range(0, len(l), 1):
-#         if l[i] < minimum:
-#             minimum = l[i]
-#         elif l[i] > maximum:
-#             maximum = l[i]
-#         total = total + l[i] 
-#         avg = total / len(l)
-#         return    
-# print(ultimate_anal([37, 2, 1, -9]))  
+def maximum(l):
+    if len(l) == 0:
+        return False
+
+    large = l[0]
+    for i in range (0, len(l), 1):
+        if l[i] > large:
+            large = l[i]
+    return large
+print(maximum([1,-3,3,-1]))
+print(maximum([])
+
+def ultimate_analysis(l):
+
+    output = {
+        "maximum": None,
+        "minimum": None, 
+        "total": None,
+        "length": None, 
+        "avg": 0, 
+    }
+    if len(l) == 0:
+        return output
+    else:
+        output["total"] = 0
+        output["maximum"] = l[0]    
+        output["minimum"] = l[0]
+
+    for i in range(0, len(l), 1):
+        if l[i] > output["maximum"]:
+            output["maximum"] = l[i]
+        elif l[i] < output["minimum"]:
+            output["minimum"] = l[i]
+
+        output["total"] = output["total"] + l[i] 
+    output["length"] = len(l)
+    output["avg"] = output["total"] / len(l)
+
+    return output    
+print(ultimate_analysis([37, 2, 1, -9])) 
+print(ultimate_analysis([])) 
 
 def reverse(l):
-    temp = l[0]
-    for i in range(len(l)):
+    for i in range(0, len(l) // 2, 1):
         temp = l[i]
-        l[i - 1] = temp
+        l[i] = l[len(l) - i - 1]
+        l[len(l) - i - 1] = temp
     return l
 print(reverse([37, 2, 1, -9]))
+
+
