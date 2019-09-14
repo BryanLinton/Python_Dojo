368db1fa015382887bf4d18b761c10da8737d9a1
Bryan <btlinton99@gmail.com>
2019-09-13 19:46:02 -0700

    loop upload

--- python_fundamentals/for_loop_basic2.py
+++ python_fundamentals/for_loop_basic2.py
@@ -36,10 +36,41 @@
 #     return long    
 # print(length([1,2,3,4]))
  
-def minimum(l):
-    small = [0]
-    for i in range (0, len(l), 1):
-        if l[i] < small:
-            small = l[i]
-    return small
-print(minimum([1,2,3,4]))                   
+# def minimum(l):
+#     small = l[0]
+#     for i in range (0, len(l), 1):
+#         if l[i] < small:
+#             small = l[i]
+#     return small
+# print(minimum([1,-3,3,-1]))  
+
+# def maximum(l):
+#     large = l[0]
+#     for i in range (0, len(l), 1):
+#         if l[i] > large:
+#             large = l[i]
+#     return large
+# print(maximum([1,-3,3,-1]))
+
+# def ultimate_anal(l):
+#     maximum = l[0]
+#     minimum = l[0]
+#     total = 0
+#     length = len(l)
+#     for i in range(0, len(l), 1):
+#         if l[i] < minimum:
+#             minimum = l[i]
+#         elif l[i] > maximum:
+#             maximum = l[i]
+#         total = total + l[i] 
+#         avg = total / len(l)
+#         return    
+# print(ultimate_anal([37, 2, 1, -9]))  
+
+def reverse(l):
+    temp = l[0]
+    for i in range(len(l)):
+        temp = l[i]
+        l[i - 1] = temp
+    return l
+print(reverse([37, 2, 1, -9]))
