35557a845cdf820f651fd7ba0e6646c4991f9d40
Bryan <btlinton99@gmail.com>
2019-09-18 12:13:24 -0700

    finished Dojo Survey didn't have time to do the challenges

--- flask/flask_fundamentals/dojo_survey/server.py
+++ flask/flask_fundamentals/dojo_survey/server.py
@@ -1,19 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/result', methods=['POST'])
+@app.route("/result", methods=["POST"])
 def survey_result():
     print("Returned Survey Info")
     print(request.form)
-    name_from_form = request.form['name']
+    name_from_form = request.form['full_name']
     location_from_form = request.form['location']
     favorite_language = request.form['language']
     comments = request.form['comments']
-    return render_template("result.html", name_from_form, location_from_form, favorite_language, comments)
+    return render_template("result.html", name = name_from_form, location = location_from_form, language = favorite_language, comment = comments)
 
 if __name__ == "__main__":
     app.run(debug=True)
