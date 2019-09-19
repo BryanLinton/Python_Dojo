45463b229f9552cd2649765016f3f43b0b1a602c
Bryan Linton <btlinton99@gmail.com>
2019-09-18 19:42:16 -0700

    updated the form_test and added the base docs for counter

--- flask/flask_fundamentals/form_test/server.py
+++ flask/flask_fundamentals/form_test/server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect # added request
-app = Flask(__name__) 
+from flask import Flask, render_template, request, redirect, session 
+app = Flask(__name__)
+app.secret_key = "keep it secret, keep it safe" 
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -7,10 +8,14 @@ def index():
 @app.route('/users', methods=['POST'])
 def create_user():
     print("Got Post Info")
-    print(request.form)
-    name_from_form = request.form['name']
-    email_from_form = request.form['email']
-    return render_template("show.html", name_on_template=name_from_form, email_on_template=email_from_form)
+    session['username'] = request.form['name']
+    session['useremail'] = request.form['email']
+    return redirect("/show")
+
+@app.route('/show')
+def show_user():
+    return render_template('show.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
