e8c6ccafc6e055ce114ae1f0905f7946a9465edd
Bryan <btlinton99@gmail.com>
2019-09-19 15:23:23 -0700

    worked on the counter homework not completed

--- flask/flask_fundamentals/counter/server.py
+++ flask/flask_fundamentals/counter/server.py
@@ -1,5 +1,21 @@
-from flask import Flask  
-app = Flask(__name__)    
+from flask import Flask, render_template, request, redirect, session  
+app = Flask(__name__) 
+app.secret_key = 'goobly glop'   
+
+@app.route('/', methods = ['GET', "POST"])
+def hello_counter():  
+    if "visits" in session:
+        session['visits'] = session.get['visits'] + 1
+        print('key exists!')
+        return render_template("index.html")
+    else:
+        print("key 'goobly glop' does not exist")
+        session['visits'] = 1
+        return render_template("index.html")
+ 
+@app.route('/counter')
+def counter():
+    return render_template("counter.html")
 
 if __name__=="__main__":      
     app.run(debug=True)    
