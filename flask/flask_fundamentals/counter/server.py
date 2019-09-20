24f6f6f856c4d70b8cfcf914f1887539588f42db
Bryan Linton <btlinton99@gmail.com>
2019-09-19 20:01:39 -0700

    finished counter project

--- flask/flask_fundamentals/counter/server.py
+++ flask/flask_fundamentals/counter/server.py
@@ -2,20 +2,22 @@ from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__) 
 app.secret_key = 'goobly glop'   
 
-@app.route('/', methods = ['GET', "POST"])
+@app.route('/')
 def hello_counter():  
     if "visits" in session:
-        session['visits'] = session.get['visits'] + 1
+        session['visits'] = int(session['visits']) + 1
         print('key exists!')
         return render_template("index.html")
     else:
-        print("key 'goobly glop' does not exist")
+        print("key doesnt exist")
         session['visits'] = 1
         return render_template("index.html")
- 
-@app.route('/counter')
-def counter():
-    return render_template("counter.html")
+
+@app.route('/destroy_session')
+def nuke_it():
+    print('boom!!')
+    session.clear()
+    return redirect('/')
 
 if __name__=="__main__":      
     app.run(debug=True)    
