eef39ebe65654a3ac8c9ad1501a77605167c5153
Bryan Linton <btlinton99@gmail.com>
2019-10-15 20:01:37 -0700

    didn't add return self to methods.  This has been fixed

--- OOP/oop_intro/oop_intro.py
+++ OOP/oop_intro/oop_intro.py
@@ -5,8 +5,10 @@ class User:
         self.account_balance = 0
     def make_deposit (self, amount):
         self.account_balance += amount
+        return self
     def make_withdrawal(self, amount):
         self.account_balance -= amount
+        return self
     def display_user_balance(self):
         print("User: {}, Balance: {}".format(self.name, self.account_balance))
         
@@ -14,7 +16,7 @@ class User:
 bubba = User("Bubba Gump", "bubbag@gmail.com")
 guido = User("Guido van Rossum", "guido@python.com")
 monty = User("Monty Python", "monty@python.com")
-bubba.make_deposit(100).make_deposit(200).make_deposit(25).bubba.make_withdrawal(325)
+bubba.make_deposit(100).make_deposit(200).make_deposit(25).make_withdrawal(325)
 guido.make_deposit(50).make_deposit(50).make_withdrawal(50).make_withdrawal(25)
 monty.make_deposit(150).make_withdrawal(50).make_withdrawal(50).make_withdrawal(50)
 bubba.display_user_balance()
