a8ddcc74c0f5093bbe62e5a258fd39e0a074ed89
Bryan <btlinton99@gmail.com>
2019-10-22 13:47:02 -0700

    next modularization assignment

--- modularization/d_n_mod/app.py
+++ modularization/d_n_mod/app.py
@@ -1,23 +1,6 @@
 from flask import Flask, render_template, request, redirect	
 from config import app, db, migrate
-from sqlalchemy.sql import func				
-
-class User(db.Model):	
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(45))
-    last_name = db.Column(db.String(45))
-    dojo_id = db.Column(db.Integer, db.ForeignKey("dojo.id"), nullable=False)
-    uDojo = db.relationship("Dojo", foreign_keys=[dojo_id], backref="user_dojo", cascade="all")
-    created_at = db.Column(db.DateTime, server_default=func.now())   
-    updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
-    
-class Dojo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    dojo_name = db.Column(db.String(45))
-    city = db.Column(db.String(45))
-    state = db.Column(db.String(45))
-    created_at = db.Column(db.DateTime, server_default=func.now())   
-    updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
+from models import User, Dojo			
 
 @app.route("/")
 def dojo_users():
